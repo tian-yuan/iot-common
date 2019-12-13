58ade5861d6ca49c6d9cb2972e367f4140ab1cf1
liuzebo <hzliuzebo@corp.netease.com>
2019-12-13 16:29:12 +0800

    tracer: add tracer plugin

---
+++ plugins/tracer/tracer.go
@@ -0,0 +1,36 @@
+package tracer
+
+import (
+	"io"
+	"time"
+
+	opentracing "github.com/opentracing/opentracing-go"
+	jaeger "github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+)
+
+func NewTracer(servicename string, addr string) (opentracing.Tracer, io.Closer, error) {
+	cfg := jaegercfg.Configuration{
+		ServiceName: servicename,
+		Sampler: &jaegercfg.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &jaegercfg.ReporterConfig{
+			LogSpans:            true,
+			BufferFlushInterval: 1 * time.Second,
+		},
+	}
+	// set Jaeger report revice address
+	sender, err := jaeger.NewUDPTransport(addr, 0)
+	if err != nil {
+		return nil, nil, err
+	}
+	// create Jaege reporter
+	reporter := jaeger.NewRemoteReporter(sender)
+	// Initialize Opentracing tracer with Jaeger Reporter
+	tracer, closer, err := cfg.NewTracer(
+		jaegercfg.Reporter(reporter),
+	)
+	return tracer, closer, err
+}
